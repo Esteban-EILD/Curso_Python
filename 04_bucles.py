d45b6f11b9a1f7672a4c9cdf717f4db785da8210
Esteban Larrea <larreadiazestebanisaac@gmail.com>
2026-05-29 13:21:37 -0500

    Corregir logica de while e implementar bucle for con range

---
+++ 04_bucles.py
@@ -0,0 +1,8 @@
+energy=100
+while energy > 0:
+    energy -= 15
+    print("\n Energía restante:", energy)
+print("\n La energía se ha agotado.")
+
+for i in range(5, 31, 5):
+    print("\n Múltiplos de 5 entre 5 y 30:", i)
