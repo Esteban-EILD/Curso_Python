d31f477171f690d63ae65214a3a0821fd1f2ae80
Esteban Larrea <larreadiazestebanisaac@gmail.com>
2026-06-03 18:08:35 -0500

    Estructuras de datos, uso de bucles y listas, desafíos

--- 05_estructuras_de_datos.py
+++ 05_estructuras_de_datos.py
@@ -26,3 +26,41 @@ valores = [0, 10, 20, 30, 40, 50, 60]
 # Extraer del índice 1 al 4 (recuerda: el 5 queda fuera)
 sub_conjunto = valores[1:5] 
 print("\nSlicing valores[1:5]:", sub_conjunto) # Debería dar [10, 20, 30, 40]
+
+# --- Desafío de Integración: Filtrado de Datos ---
+print("\n--- Procesamiento de Señales ---")
+
+voltajes = [120, 85, 230, 115, 50, 195, 240]
+voltajes_estables = []
+
+# Tu tarea: 
+# Recorre la lista de 'voltajes' usando un bucle for.
+# Si el voltaje se encuentra en el rango estable (entre 110 y 220 inclusive), 
+# añádelo a la lista 'voltajes_estables' usando el método .append().
+# Al final, imprime la lista de voltajes estables filtrados.
+for v_estable in voltajes:
+    if 110 <= v_estable <= 220:
+        voltajes_estables.append(v_estable)
+
+print("Voltajes estables:", voltajes_estables)
+
+# --- Desafío de Integración: Limpieza y Clasificación de Datos ---
+datos_sucios = [65, "error", 88, 42, "nulo", 95, 30, "revisar", 71]
+
+notas_aprobatorias = []
+notas_reprobatorias = []
+
+for elemento in datos_sucios:
+    if isinstance(elemento, int):
+        # Sumamos 5 puntos asegurando un tope máximo de 100
+        nota_con_bono = min(elemento + 5, 100)
+        
+        # Clasificamos directamente en el mismo bucle
+        if nota_con_bono >= 60:
+            notas_aprobatorias.append(nota_con_bono)
+        else:
+            notas_reprobatorias.append(nota_con_bono)
+
+# Imprimimos una sola vez al final del programa
+print("Notas aprobatorias:", notas_aprobatorias)
+print("Notas reprobatorias:", notas_reprobatorias)
